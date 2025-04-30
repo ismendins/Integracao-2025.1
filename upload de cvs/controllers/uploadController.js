6f7ba4a19bd6930cefa3e63a5cd12f30d2f0c503
ALUNO C32 <alunoc32@acad.unifor.br>
2025-04-30 20:24:39 -0300

    finished implanting upload of users

--- upload de cvs/controllers/uploadController.js
+++ upload de cvs/controllers/uploadController.js
@@ -1,7 +1,30 @@
 const supabase = require('../config/db');
+const csv = require('csv-parser');
+const fs = require('fs');
+//const path = require('path');
 
 const uploadFunc = async (req, res) => {
-    res.send('Hello World!');
+   const filePath = req.file.path;
+   const results = [];
+
+   fs.createReadStream(filePath)
+   .pipe(csv())
+   .on('data', (data)=> results.push(data))
+   .on('end', async ()=> {
+    try{
+        const {data, error } = await supabase.from('usuario').insert(results);
+        if (error){
+            console.log(error);
+            return res.status(500).send("Erro de inserção de dados no supabase");
+        }
+        console.log('Upload realizado com sucesso. ', data)
+    } catch (err){
+        console.log(err);
+        res.status(500).send("Erro interno");
+    } finally {
+        fs.unlinkSync(filePath);
+    }
+   }) 
 };
 
 module.exports = { uploadFunc };
--- upload de cvs/routes/uploadRoutes.js
+++ upload de cvs/routes/uploadRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
 const uploadControl = require('../controllers/uploadController.js');
 
+const upload = multer({dest :'upload/'});
 
 router.get('/', (req, res) => {
     res.render('index.ejs');
 })
 
-router.post('/upload', uploadControl.uploadFunc);
+router.post('/upload', upload.single('file'), uploadControl.uploadFunc);
 
 module.exports = router;
